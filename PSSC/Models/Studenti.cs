71a8494eb845fa81a180ac8414269ecc3fc648d1
AFlavius <ardelean.flavius.cosmin@gmail.com>
2016-10-09 18:51:12 +0300

    Commit1

---
+++ PSSC/Models/Studenti.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Studenti :Facultate
+    {
+        string nume;
+        string prenume;
+        int varsta;
+        string CNP;
+        string data_nasterii;
+        float[] medii;
+        string nr_matricol;
+    }
+}
